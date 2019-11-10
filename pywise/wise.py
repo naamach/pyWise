b22e8591a8a565fdcb43d1f83638e198d3953175
Na'ama Hallakoun <naama@wise.tau.ac.il>
2019-11-10 16:33:00 +0200

    return reduced_path

--- pywise/wise.py
+++ pywise/wise.py
@@ -112,6 +112,6 @@ def reduce_nights(d1, d2=None, telescope="C28", config_file="config.ini"):
     """
     d1 and d2 should be in the format "YYYYMMDD"
     """
-    reduced_path = daterange_func(d1, d2, func=reduce_night, telescope=telescope, config_file=config_file)
+    daterange_func(d1, d2, func=reduce_night, telescope=telescope, config_file=config_file)
 
-    return reduced_path
+    return
